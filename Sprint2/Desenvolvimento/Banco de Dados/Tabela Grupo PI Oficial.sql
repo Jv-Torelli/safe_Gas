c957061f5a6c1ac24e2a3e1c3fcd6e53928cbbfd
Jv-Torelli <joao.vmatos@sptech.school>
2025-04-14 14:22:31 -0300

    Alteração adm no banco de dados

--- Sprint2/Desenvolvimento/Banco de Dados/Tabela Grupo PI Oficial.sql
+++ Sprint2/Desenvolvimento/Banco de Dados/Tabela Grupo PI Oficial.sql
@@ -2,7 +2,13 @@ CREATE DATABASE sgc;
 USE sgc;
 
 
-CREATE TABLE condominio (
+CREATE TABLE adm(
+idAdm INT PRIMARY KEY auto_increment,
+cnpj VARCHAR(14),
+senha VARCHAR(8)
+);
+
+CREATE TABLE cadastroCondominio (
 idCondominio INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 logradouro VARCHAR(50) NOT NULL,
@@ -63,7 +69,10 @@ CONSTRAINT fkCondominio FOREIGN KEY (fkCondominio) references condominio(idCondo
 );
 
 
-INSERT INTO condominio VALUES
+INSERT INTO adm(cnpj, senha)
+VALUES ('12312312312344', '@1234567');
+
+INSERT INTO cadastroCondominio VALUES
 (DEFAULT, 'Condomínio Coelho', 'Rua Baronesa de Itu', '1231001', 175, '12435278000193', 'coelho.cond@outlook.com', 'segccl01'),
 (DEFAULT, 'Condomínio Campestre', 'Rua Dona Veridiana', '01238010', 50, '13629658000194', 'campestre.cond@outlook.com', 'segcpt02'),
 (DEFAULT, 'Condomínio Ametista', 'Rua Doutor Nicolau de Sousa Queirós', '4105002', 730, '12569812000195', 'ametista.cond@outlook.com', 'segamt03'),
@@ -77,13 +86,6 @@ VALUES('Normal'),
 ('Crítico'),
 ('Crítico');
 
-
-INSERT INTO usuario VALUES
-(DEFAULT, '12435278000193', 'segccl01'),
-(DEFAULT, '13629658000194', 'segcpt02'),
-(DEFAULT, '12569812000195', 'segamt03'),
-(DEFAULT, '14365878000192', 'segclb04'),
-(DEFAULT, '14637498000190', 'segfnx05');
 
 INSERT INTO sensor (nome, tipo, statusSensor) VALUES
 ('MQ-2', 'Sensor gás', 'Ativo'),
