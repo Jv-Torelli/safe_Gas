6704b1ae4e9a40d6bb5a10c4f5d5625bbfe93c0b
Jv-Torelli <joao.vmatos@sptech.school>
2025-04-22 21:09:53 -0300

    Agora sim o script ta atualizado

--- Sprint2/Desenvolvimento/Banco de Dados/Tabela Grupo PI Oficial.sql
+++ Sprint2/Desenvolvimento/Banco de Dados/Tabela Grupo PI Oficial.sql
@@ -2,45 +2,45 @@ CREATE DATABASE sgc;
 USE sgc;
 
 
-CREATE TABLE adm(
-idAdm INT PRIMARY KEY auto_increment,
-cnpj VARCHAR(14),
-senha VARCHAR(8)
-);
-
-CREATE TABLE cadastroCondominio (
+CREATE TABLE condominio (
 idCondominio INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
+logradouro VARCHAR(50) NOT NULL,
 cep CHAR(8) NOT NULL,
 numero INT NOT NULL,
-bairro VARCHAR(45)NOT NULL,
 cnpj CHAR(14) UNIQUE,
 email VARCHAR(40),
 senha CHAR(8) NOT NULL
 );
 
-CREATE TABLE apartamento(
-idApartamento INT PRIMARY KEY auto_increment,
-nomeCondomino VARCHAR(45),
-numApartamento VARCHAR(5),
-metragem INT, 
-andar INT,
-fkCondomino INT,
-CONSTRAINT fkCondomino FOREIGN KEY (fkCondomino) references apartamento(idApartamento),
-fkCadastroCondominio INT,
-CONSTRAINT fkCadastroCondominio FOREIGN KEY (fkCadastroCondominio) references condominio(condominio)
+CREATE TABLE login (
+idLogin INT PRIMARY KEY AUTO_INCREMENT,
+cnpj CHAR(14) UNIQUE NOT NULL,
+senha VARCHAR(8) NOT NULL,
+fkCondominio INT,
+CONSTRAINT fkCondominio FOREIGN KEY (fkCondominio) references condominio(idCondominio)
 );
-
 
 CREATE TABLE sensor (
 idSensor INT PRIMARY KEY auto_increment,
 nome VARCHAR(25) NOT NULL,
 tipo VARCHAR(30) NOT NULL,
+apartamento VARCHAR(5) NOT NULL,
 statusSensor VARCHAR(15),
 	CONSTRAINT ckSensor 
 		CHECK (statusSensor IN ('Ativo', 'Inativo')),
-fkApartamento INT,
-CONSTRAINT fkApartamento FOREIGN KEY (fkApartamento) references apartamento(idApartamento)
+fkCondominioSensor INT,
+CONSTRAINT fkCondominioSensor FOREIGN KEY (fkCondominioSensor) references condominio(idCondominio)
+);
+
+CREATE TABLE apartamento (
+idApartamento INT PRIMARY KEY auto_increment,
+blocoApartamento VARCHAR(3),
+numApartamento VARCHAR(5),
+metragem INT,
+andar INT,
+fkCondominioApartamento INT,
+CONSTRAINT fkCondominioApartamento FOREIGN KEY (fkCondominioApartamento) references condominio(idCondominio)
 );
 
 
@@ -48,31 +48,29 @@ CREATE TABLE alerta(
 idAlerta INT PRIMARY KEY auto_increment,
 statusAlerta VARCHAR(30),
 	CONSTRAINT ckStatus
-		CHECK (statusAlerta IN ('Normal', 'Atenção', 'Crítico')),
-fkSensor INT,
-CONSTRAINT fkSensor FOREIGN KEY (fkSensor) references sensor(idSensor)
+		CHECK (statusAlerta IN ('Normal', 'Atenção', 'Crítico'))
 );
 
 CREATE TABLE medicao (
 idMedicao INT PRIMARY KEY AUTO_INCREMENT,
 dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-concentracaoGases FLOAT NOT NULL
+concentracaoGases FLOAT NOT NULL,
+	CONSTRAINT ckTaxa 
+		CHECK (concentracaoGases IN (5,10,20)),
+fkSensor INT,
+CONSTRAINT fkSensor FOREIGN KEY (fkSensor) references sensor(idSensor)
 );
 
 CREATE TABLE contato(
 idContato INT PRIMARY KEY auto_increment,
-assunto VARCHAR(40),
+assunto VARCHAR(60),
 email VARCHAR(80),
 mensagem VARCHAR(200),
-fkCondominio INT,
-CONSTRAINT fkCondominio FOREIGN KEY (fkCondominio) references condominio(idCondominio)
+fkCondominioContato INT,
+CONSTRAINT 	fkCondominioContato FOREIGN KEY (fkCondominioContato) references condominio(idCondominio)
 );
 
-
-INSERT INTO adm(cnpj, senha)
-VALUES ('12312312312344', '@1234567');
-
-INSERT INTO cadastroCondominio VALUES
+INSERT INTO condominio VALUES
 (DEFAULT, 'Condomínio Coelho', 'Rua Baronesa de Itu', '1231001', 175, '12435278000193', 'coelho.cond@outlook.com', 'segccl01'),
 (DEFAULT, 'Condomínio Campestre', 'Rua Dona Veridiana', '01238010', 50, '13629658000194', 'campestre.cond@outlook.com', 'segcpt02'),
 (DEFAULT, 'Condomínio Ametista', 'Rua Doutor Nicolau de Sousa Queirós', '4105002', 730, '12569812000195', 'ametista.cond@outlook.com', 'segamt03'),
@@ -86,6 +84,13 @@ VALUES('Normal'),
 ('Crítico'),
 ('Crítico');
 
+
+INSERT INTO usuario VALUES
+(DEFAULT, '12435278000193', 'segccl01'),
+(DEFAULT, '13629658000194', 'segcpt02'),
+(DEFAULT, '12569812000195', 'segamt03'),
+(DEFAULT, '14365878000192', 'segclb04'),
+(DEFAULT, '14637498000190', 'segfnx05');
 
 INSERT INTO sensor (nome, tipo, statusSensor) VALUES
 ('MQ-2', 'Sensor gás', 'Ativo'),
@@ -110,8 +115,6 @@ concentracaoGases AS 'Concentração de Gases',
 statusAlerta AS 'Status'
 FROM medicao;
 
-select * from medicao;
-
 SELECT * FROM condominio WHERE logradouro LIKE '%L%';
 
 SELECT * FROM sensor WHERE statusSensor NOT LIKE 'Inativo';
@@ -123,5 +126,3 @@ SELECT * FROM medicao WHERE concentracaoGases <= 10;
 SELECT * FROM condominio WHERE logradouro LIKE '%T%' OR logradouro LIKE '%M%';
 
 SELECT nome AS 'Nome', CONCAT(logradouro, ', ', numero) AS 'Endereço Completo', cep AS 'CEP' FROM condominio;
-
-update medicao SET concentracaoGases = 200 where idMedicao = 1;
