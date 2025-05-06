6071d841a6df0e3ab29595f5abe314ecb73708bc
HygorSW <Hygor.Wanderlei@sptech.school>
2025-05-06 00:12:46 -0300

    Login e cadastro integrado com o banco de dados - INSERÇÃO E MELHORIA

--- Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/controllers/usuarioController.js
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/controllers/usuarioController.js
@@ -2,54 +2,40 @@ var usuarioModel = require("../models/usuarioModel");
 var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var cnpj = req.body.cnpjServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
+    usuarioModel.autenticar(cnpj, senha)
+    .then(
+        function (resultadoAutenticar) {
+            console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+            console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-        usuarioModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+            if (resultadoAutenticar.length == 1) {
+                console.log(resultadoAutenticar);
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
+                res.json({
+                    idUsuario: resultadoAutenticar[0].idUsuario,
+                    nome: resultadoAutenticar[0].nome,
+                    cnpj: resultadoAutenticar[0].cnpj
+                });
+            } else if (resultadoAutenticar.length == 0) {
+                res.status(403).send("cnpj e/ou senha inválido(s)");
+            } else {
+                res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+            }
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+        }
+    ).catch(
+        function (erro) {
+            console.log(erro);
+            console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        }
+    );
     }
 
-}
+
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
@@ -64,13 +50,13 @@ function cadastrar(req, res) {
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (cep == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu cep está undefined!");
     } else if (logradouro == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu logradouro está undefined!");
     } else if (numLog == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu numLog está undefined!");
     } else if (cnpj == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu cnpj está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
