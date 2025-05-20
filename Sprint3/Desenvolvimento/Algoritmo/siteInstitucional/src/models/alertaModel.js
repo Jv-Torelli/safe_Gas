4b4d85250b9e9f701a190816ca4000e5f27de70e
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-05-19 22:52:00 -0300

    alertaModel adionada para funcao à  dashboard especifica

---
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/alertaModel.js
@@ -0,0 +1,18 @@
+var database = require("../database/config")
+
+function retornar_dados_kpi(numero_apartamento) {
+    var instrucaoSql = `
+    SELECT statusAlerta, acao, risco 
+        FROM alerta
+        JOIN apartamento
+        ON alerta.fkApartamentoMedicao = apartamento.idApartamento
+        WHERE apartamento.numero_apartamento = '${numero_apartamento}';`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+  }
+
+
+module.exports = {
+    retornar_dados_kpi,
+};
