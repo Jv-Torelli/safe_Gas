6ad6727d99545c5588e7da9884c597d787cb6860
HygorSW <Hygor.Wanderlei@sptech.school>
2025-06-05 12:24:16 -0300

    alteraões na estilização dashboard geral.

--- Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/alertaModel.js
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/alertaModel.js
@@ -2,11 +2,7 @@ var database = require("../database/config")
 
 function retornar_dados_kpi(numero_apartamento) {
     var instrucaoSql = `
-    SELECT statusAlerta, acao, risco 
-        FROM alerta
-        JOIN apartamento
-        ON alerta.fkApartamentoMedicao = apartamento.idApartamento
-        WHERE apartamento.numero_apartamento = '${numero_apartamento}';`;
+   select * from notificacoes where Numero_Apartamento = ${numero_apartamento};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
