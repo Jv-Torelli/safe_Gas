97dd1dc3e521d862b34290fb6f74f4593745fc3f
Jv-Torelli <joao.vmatos@sptech.school>
2025-06-10 23:36:57 -0300

    Acesso desmistificado entre portaria e sindico

--- Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/controllers/usuarioController.js
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/controllers/usuarioController.js
@@ -1,43 +1,66 @@
 var usuarioModel = require("../models/usuarioModel");
 
 function autenticar(req, res) {
-    var cnpj = req.body.cnpjServer;
+    var identificador = req.body.identificadorServer;
     var senha = req.body.senhaServer;
+    var tipoUsuario = req.body.tipoUsuarioServer;
 
-    usuarioModel.autenticar(cnpj, senha)
-    .then(
-        function (resultadoAutenticar) {
+    
+    if (!identificador || !senha || !tipoUsuario) {
+        res.status(400).send("Dados incompletos para autenticação");
+        return;
+    }
+
+   
+    if (tipoUsuario !== 'sindico' && tipoUsuario !== 'porteiro') {
+        res.status(400).send("Tipo de usuário inválido");
+        return;
+    }
+
+    console.log(`Tentativa de login - Tipo: ${tipoUsuario}, Identificador: ${identificador}`);
+
+    usuarioModel.autenticar(identificador, senha, tipoUsuario)
+        .then(function (resultadoAutenticar) {
             console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-            console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+            console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
             if (resultadoAutenticar.length == 1) {
                 console.log(resultadoAutenticar);
 
-                res.json({
-                    idUsuario: resultadoAutenticar[0].idUsuario,
-                    nome: resultadoAutenticar[0].nome,
-                    cnpj: resultadoAutenticar[0].cnpj
-                });
+                const usuario = resultadoAutenticar[0];
+                
+                if (tipoUsuario === 'sindico') {
+                    res.json({
+                        id: usuario.idCondominio,
+                        nome: usuario.nome_condominio,
+                        cnpj: usuario.cnpj,
+                        tipo: 'sindico'
+                    });
+                } else { 
+                    res.json({
+                        id: usuario.idPortaria,
+                        nome: `Portaria ${usuario.numero_portaria}`,
+                        email: usuario.email,
+                        fkCondominio: usuario.fkCondominioPortaria,
+                        tipo: 'porteiro'
+                    });
+                }
+
             } else if (resultadoAutenticar.length == 0) {
-                res.status(403).send("cnpj e/ou senha inválido(s)");
+                res.status(403).send("Credenciais inválidas");
             } else {
-                res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                res.status(403).send("Múltiplos usuários encontrados com as mesmas credenciais");
             }
 
-        }
-    ).catch(
-        function (erro) {
+        })
+        .catch(function (erro) {
             console.log(erro);
             console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
             res.status(500).json(erro.sqlMessage);
-        }
-    );
-    }
-
-
+        });
+}
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var cep = req.body.cepServer;
     var logradouro = req.body.logradouroServer;
@@ -45,7 +68,6 @@ function cadastrar(req, res) {
     var cnpj = req.body.cnpjServer;
     var senha = req.body.senhaServer;
 
-    // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (cep == undefined) {
@@ -60,22 +82,19 @@ function cadastrar(req, res) {
         res.status(400).send("Sua senha está undefined!");
     } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        
         usuarioModel.cadastrar(nome, cep, logradouro, numLog, cnpj, senha)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+            .then(function (resultado) {
+                res.json(resultado);
+            })
+            .catch(function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            });
     }
 }
 
--- Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/usuarioModel.js
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/usuarioModel.js
@@ -1,22 +1,38 @@
 var database = require("../database/config")
 
-function autenticar(cnpj, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", cnpj, senha)
-    var instrucaoSql = `
-        SELECT idCondominio, nome_condominio, cnpj, senha FROM condominio WHERE cnpj = '${cnpj}' AND senha = '${senha}';
-    `;
+function autenticar(identificador, senha, tipoUsuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", identificador, senha, tipoUsuario);
+    
+    var instrucaoSql = "";
+    
+    if (tipoUsuario === 'sindico') {
+        
+        instrucaoSql = `
+            SELECT idCondominio, nome_condominio, cnpj, senha 
+            FROM condominio 
+            WHERE cnpj = '${identificador}' AND senha = '${senha}';
+        `;
+    } else if (tipoUsuario === 'porteiro') {
+        
+        instrucaoSql = `
+            SELECT idPortaria, numero_portaria, telefone, email, senha, fkCondominioPortaria
+            FROM portaria 
+            WHERE email = '${identificador}' AND senha = '${senha}';
+        `;
+    } else {
+        throw new Error("Tipo de usuário inválido");
+    }
+    
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, cep, logradouro, numero_log, cnpj, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, cep, logradouro, numero_log, cnpj, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO condominio (nome_condominio, cep, logradouro, numero_logradouro, cnpj, senha) VALUES ('${nome}', '${cep}', '${logradouro}', '${numero_log}', '${cnpj}',  '${senha}');
+        INSERT INTO condominio (nome_condominio, cep, logradouro, numero_logradouro, cnpj, senha) 
+        VALUES ('${nome}', '${cep}', '${logradouro}', '${numero_log}', '${cnpj}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
